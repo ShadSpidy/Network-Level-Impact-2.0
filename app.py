9bdb961ddca31e9912912bfa6ca6f07e20b1c333
Shadman Akhter <shadmanakhter78@gmail.com>
2024-08-28 02:58:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ try:
     for row in range(118, 134):
         node_name = npi_report.cell(row, 3).value.upper().replace(' ', '')
         traffic = npi_report.cell(row, 7).value
-        if traffic is not None:
-            node_wise_traffic[node_name] = traffic
+        node_wise_traffic[node_name] = traffic
 
     total_traffic = sum(node_wise_traffic.values())
 
@@ -33,22 +32,21 @@ try:
     # User input for impacted nodes
     user_input = st.text_input("Please enter the impacted node(s) (separate multiple nodes with commas):").upper()
 
-    if st.button('Calculate Impact'):
-        if user_input:
-            impacted_nodes = [node.strip() for node in user_input.split(',')]
-            impacted_node_traffic = sum(node_wise_traffic.get(node, 0) for node in impacted_nodes)
+    if user_input:  # Check if user_input is not empty
+        impacted_nodes = [node.strip() for node in user_input.split(',')]
+        impacted_node_traffic = sum(node_wise_traffic.get(node, 0) for node in impacted_nodes)
 
-            if impacted_node_traffic == 0:
-                st.error("Invalid input or no valid nodes entered. Please enter valid node names.")
-            else:
-                # User input for current percentage impact
-                percentage_impact = st.number_input("Enter the current percentage impact (%):", min_value=0.0, max_value=100.0, step=0.1)
-
-                # Calculate network level impact
-                NW_level_impact = ((impacted_node_traffic / total_traffic) * 100) * (percentage_impact / 100)
-                st.write(f"Current Network Level Impact is {round(NW_level_impact, 2)}%")
+        if impacted_node_traffic == 0:
+            st.error("Invalid input or no valid nodes entered. Please enter valid node names.")
         else:
-            st.info("Please enter the impacted node(s) to get results.")
+            # User input for current percentage impact
+            percentage_impact = st.number_input("Enter the current percentage impact (%):", min_value=0.0, max_value=100.0, step=0.1)
+
+            # Calculate network level impact
+            NW_level_impact = ((impacted_node_traffic / total_traffic) * 100) * (percentage_impact / 100)
+            st.write(f"Current Network Level Impact is {round(NW_level_impact, 2)}%")
+    else:
+        st.info("Please enter the impacted node(s) to get results.")
 
 except Exception as e:
     st.error(f"An error occurred: {e}")
