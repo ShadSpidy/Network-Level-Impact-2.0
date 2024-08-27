1ba11c155cd857b31d72f61084db12bf33c46bc8
Shadman Akhter <shadmanakhter78@gmail.com>
2024-08-28 02:37:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import openpyxl
 import requests
 from io import BytesIO
+import streamlit as st
 
 # Raw URL of the file on GitHub
 file_url = "https://github.com/ShadSpidy/Network-Level-Impact-2.0/raw/main/drc_npi.xlsx"
@@ -26,25 +27,26 @@ try:
     total_traffic = sum(node_wise_traffic.values())
 
     # Streamlit app
-    import streamlit as st
     st.title("Network Impact Calculator")
 
     # User input for impacted nodes
     user_input = st.text_input("Please enter the impacted node(s) (separate multiple nodes with commas):").upper()
-    impacted_nodes = [node.strip() for node in user_input.split(',')]
 
-    # Calculate traffic for impacted nodes
-    impacted_node_traffic = sum(node_wise_traffic.get(node, 0) for node in impacted_nodes)
+    if user_input:  # Check if user_input is not empty
+        impacted_nodes = [node.strip() for node in user_input.split(',')]
+        impacted_node_traffic = sum(node_wise_traffic.get(node, 0) for node in impacted_nodes)
 
-    if impacted_node_traffic == 0:
-        st.error("Invalid input or no valid nodes entered. Please enter valid node names.")
+        if impacted_node_traffic == 0:
+            st.error("Invalid input or no valid nodes entered. Please enter valid node names.")
+        else:
+            # User input for current percentage impact
+            percentage_impact = st.number_input("Enter the current percentage impact (%):", min_value=0.0, max_value=100.0, step=0.1)
+
+            # Calculate network level impact
+            NW_level_impact = ((impacted_node_traffic / total_traffic) * 100) * (percentage_impact / 100)
+            st.write(f"Current Network Level Impact is {round(NW_level_impact, 2)}%")
     else:
-        # User input for current percentage impact
-        percentage_impact = st.number_input("Enter the current percentage impact (%):", min_value=0.0, max_value=100.0, step=0.1)
-
-        # Calculate network level impact
-        NW_level_impact = ((impacted_node_traffic / total_traffic) * 100) * (percentage_impact / 100)
-        st.write(f"Current Network Level Impact is {round(NW_level_impact, 2)}%")
+        st.info("Please enter the impacted node(s) to get results.")
 
 except Exception as e:
-    print(f"An error occurred: {e}")
+    st.error(f"An error occurred: {e}")
